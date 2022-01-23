d2962427ecd857fad5f8fd3d81231ae9a86e62f6
Amol Bais <amolsanjay.bais.ext@bpost.be>
2022-01-23 23:23:53 +0530

    Cyclic Permutations

---
+++ Strings/CyclicPermutations.java
@@ -0,0 +1,16 @@
+public class CyclicPermutations {
+    public int solve(String A, String B) {
+        int n=A.length();
+        // append the same string so that
+        // we can use sliding window to find the cyclic permutation
+        B += B;
+        int count = 0;
+
+        for(int i=0; i<A.length(); i++) {
+            // slide through B and compare with A
+            if(B.substring(i,i+n).equals(A)) count++;
+        }
+
+        return count;
+    }
+}
