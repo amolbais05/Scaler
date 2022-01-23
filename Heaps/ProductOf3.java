a39fc6cd1d9adb752fde87fd4e251f243bbd5ee9
Amol Bais <amolsanjay.bais.ext@bpost.be>
2022-01-23 22:28:30 +0530

    Product of 3

--- Heaps/ProductOf3.java
+++ Heaps/ProductOf3.java
@@ -1,2 +1,36 @@
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+
 public class ProductOf3 {
+
+    public ArrayList<Integer> solve(ArrayList<Integer> A)
+    {
+
+        int N = A.size();
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        ArrayList<Integer> ans = new ArrayList<>();
+
+        for(int i = 0 ; i < N ; i++)
+        {
+            pq.add(A.get(i));
+            if(i < 2)
+            {
+                ans.add(-1);
+            }
+            else
+            {
+                int a = pq.peek();
+                pq.poll();
+                int b = pq.peek();
+                pq.poll();
+                int c = pq.peek();
+                pq.poll();
+                pq.add(a);
+                pq.add(b);
+                pq.add(c);
+                ans.add(a*b*c);
+            }
+        }
+        return ans;
+    }
 }
