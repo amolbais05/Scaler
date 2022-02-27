1da70884ad889e0718591d82984ff514013d4742
Amol Bais <amolsanjay.bais.ext@bpost.be>
2022-02-27 15:09:32 +0530

    MinXORvalue

---
+++ Bit Manipulation/MinXORvalue.java
@@ -0,0 +1,51 @@
+import java.util.Arrays;
+
+/**
+ Problem Description:
+ Given an integer array A of N integers, find the pair of integers in the array which have minimum XOR value.
+ Report the minimum XOR value.
+
+ Problem Constraints:
+ 2 <= length of the array <= 100000
+ 0 <= A[i] <= 109
+
+ Input Format:
+ First and only argument of input contains an integer array A.
+
+ Output Format:
+ Return a single integer denoting minimum xor value.
+
+ Example Input:
+ Input 1:
+ A = [0, 2, 5, 7]
+
+ Input 2:
+ A = [0, 4, 7, 9]
+
+ Example Output
+ Output 1:
+ 2
+
+ Output 2:
+ 3
+
+ Example Explanation
+ Explanation 1:
+ 0 xor 2 = 2
+ **/
+
+
+public class MinXORvalue {
+    public int findMinXor(int[] A) {
+
+        int n=A.length;
+        Arrays.sort(A);
+        int min=A[0]^A[1];
+        for(int i=2;i<n;i++){
+            min=Math.min(min,A[i]^A[i-1]);
+        }
+        return min;
+
+
+    }
+}
