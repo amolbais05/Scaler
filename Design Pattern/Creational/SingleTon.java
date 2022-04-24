8aab73ca25f968b17a6bdb4ba006dac25047303b
Amol Bais <amolsanjay.bais.ext@bpost.be>
2022-04-24 22:22:51 +0530

    Singleton example

---
+++ Design Pattern/Creational/SingleTon.java
@@ -0,0 +1,28 @@
+public class SingleTon {
+
+    private static SingleTon singleTon = null;
+
+    private SingleTon() {}
+
+    public static SingleTon getInstance()
+    {
+        if (null == singleTon)
+        {
+            synchronized (SingleTon.class)
+            {
+                if (null == singleTon)
+                {
+                    singleTon= new SingleTon();
+                }
+            }
+        }
+
+        return singleTon;
+    }
+
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+}
---
+++ Design Pattern/Creational/TestSinglton.java
@@ -0,0 +1,9 @@
+public class TestSinglton {
+
+    public static void main(String[] args) {
+
+        SingleTon singleTon = SingleTon.getInstance();
+
+
+    }
+}
